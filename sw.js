e9a3ca6bbf039e3b4e76e56d046809a251ba5eb5
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-08-05 11:27:43 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "assessor-plus-v0.2";
+const CACHE_NAME = "assessor-plus-v0.3";
 const APP_SHELL = [
   "./",
   "./index.html",
@@ -6,7 +6,8 @@ const APP_SHELL = [
   "./app.js",
   "./manifest.webmanifest",
   "./icon-192.png",
-  "./icon-512.png"
+  "./icon-512.png",
+  "./assessor-logo.png"
 ];
 
 self.addEventListener("install", event => {
